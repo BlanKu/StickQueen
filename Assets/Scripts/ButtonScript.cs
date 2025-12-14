d9c9cc7dc20bc396cefb31155da216f4c8530907
Jakub Rosiński <jakub.rosinski2k4@gmail.com>
2025-12-14 16:17:24 +0100

    Daily Update

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.SearchService;
 using UnityEngine;
@@ -6,14 +7,34 @@ using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
+    [Header("Change Scene Button")]
     public string sceneNameToGo;
+
+    [Header("Show/Hide Objects Button")]
+    public List<GameObject> ShowHideObjects;
+
+    [Header("Quit Game Button")]
+    public bool isQuitButton;
+
 
     private Button _button;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _button = GetComponent<Button>();
-        _button.onClick.AddListener(ChangeScene);
+
+        if (sceneNameToGo != null)
+        {
+            _button.onClick.AddListener(ChangeScene);
+        }
+        else if (isQuitButton)
+        {
+            _button.onClick.AddListener(ClickExitButton);
+        }
+        else if (ShowHideObjects != null)
+        {
+            _button.onClick.AddListener(ShowOrHideObejects);
+        }
     }
 
     // Update is called once per frame
@@ -25,5 +46,25 @@ public class ButtonScript : MonoBehaviour
     void ChangeScene()
     {
         SceneManager.LoadScene(sceneNameToGo);
+    }
+
+    void ClickExitButton()
+    {
+        Application.Quit();
+    }
+
+    void ShowOrHideObejects()
+    {
+        for (int i = 0; i < ShowHideObjects.Count; i++)
+        {
+            if(ShowHideObjects[i].active == false)
+            {
+                ShowHideObjects[i].SetActive(true);
+            }
+            else
+            {
+                ShowHideObjects[i].SetActive(false);
+            }
+        }
     }
 }
